7958a38387e119385bfe59b9c2996359b7e2161a
Vishnu <vpvishnu2000@gmail.com>
2021-11-12 23:42:52 +0530

    Final commit

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,12 +3,8 @@ from django import forms
 from django.contrib.auth.models import User
 
 
-
-
 class UserUpdate(forms.ModelForm):
     username = forms.CharField(required=True, max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Enter your Username'}))
-    
-
 
     class Meta:
         model = User
